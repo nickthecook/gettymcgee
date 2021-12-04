25a392e726535992a30c55cec72ddc838e716908
nickthecook <nickthecook@gmail.com>
2021-12-04 12:14:05 -0500

    configure default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :cloud_files
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "cloud_files#index"
 end
