a53927f5286c801bcbf11fce637f76114db494e6
nickthecook <nickthecook@gmail.com>
2021-12-19 10:55:58 -0500

    add rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ require 'sidekiq/web'
 require 'sidekiq/cron/web'
 
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :cloud_files
   post "/cloud_files/add", to: "cloud_files#add"
   delete "/cloud_files/delete/:id", to: "cloud_files#delete", as: "cloud_files_delete"
